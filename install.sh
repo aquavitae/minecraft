394dd575c8126347da142daf3e7565aee4131f47
David Townshend <aquavitae69@gmail.com>
2022-08-21 13:29:02 +0200

    remove debugging

--- install.sh
+++ install.sh
@@ -13,10 +13,6 @@ rm -rf "$target/behavior_packs/Pets"
 rm -rf "$target/behavior_packs/dragons.v14"
 rm -rf "$target/resource_packs/dragons.v14"
 
-pwd
-ls
-ls $target
-
 cp -r ./behavior "$target/behavior_packs/Pets"
 cp -r ./addons/dragons.v14/behavior "$target/behavior_packs/dragons.v14"
 cp -r ./addons/dragons.v14/resource "$target/resource_packs/dragons.v14"
