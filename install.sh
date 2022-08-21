5e0084f3f5931d42b5cea4db7c01b3af85b8e7e1
David Townshend <aquavitae69@gmail.com>
2022-08-21 14:08:50 +0200

    more settings to install

--- install.sh
+++ install.sh
@@ -17,6 +17,10 @@ cp -r ./behavior "$target/behavior_packs/Pets"
 cp -r ./addons/dragons.v14/behavior "$target/behavior_packs/dragons.v14"
 cp -r ./addons/dragons.v14/resource "$target/resource_packs/dragons.v14"
 
+cp -r ./world/* "$target/worlds/Legoland"
+
+cp ./settings/* "$target"
+
 cd $HOME
 rm -rf minecraft
 
