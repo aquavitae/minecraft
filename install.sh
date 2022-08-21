c20a755f816113a51f01dfbe8b7614e6a61a6a2c
David Townshend <aquavitae69@gmail.com>
2022-08-21 13:27:01 +0200

    debugging

--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 set -e -o pipefail
+set -v
 
-rm -rf ~/minecraft
+rm -rf $HOME/minecraft
 git clone https://github.com/aquavitae/minecraft.git
-cd ~/minecraft
+cd $HOME/minecraft
 
-target="~/minecraftbe/mycraft"
+target="$HOME/minecraftbe/mycraft"
 
 rm -rf "$target/behavior_packs/Pets"
 rm -rf "$target/behavior_packs/dragons.v14"
 rm -rf "$target/resource_packs/dragons.v14"
 
+pwd
+ls
+ls $target
+
 cp -r ./behavior "$target/behavior_packs/Pets"
 cp -r ./addons/dragons.v14/behavior "$target/behavior_packs/dragons.v14"
 cp -r ./addons/dragons.v14/resource "$target/resource_packs/dragons.v14"
 
-cd ~
+cd $HOME
 rm -rf minecraft
 
 $target/restart.sh
