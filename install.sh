ee49bf491bca5fb8403b9ee14eb9dd14e1e8cbda
David Townshend <aquavitae69@gmail.com>
2022-08-21 13:06:49 +0200

    full install

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo "I ran"
-# -C 'git clone https://github.com/aquavitae/minecraft.git -C /tmp/minecraft'
-#           ssh -i oracle.key -C 'cd /tmp/minecraft
+set -e -o pipefail
+
+rm -rf ~/my-minecraft
+git clone https://github.com/aquavitae/minecraft.git -C ~/my-minecraft
+cd ~/my-minecraft
+
+target="~/minecraftbe/mycraft"
+
+rm -rf "$target/behavior_packs/*"
+rm -rf "$target/resource_packs/*"
+
+cp -r ./behavior "$target/behavior_packs/Pets"
+cp -r ./addons/dragons.v14/behavior "$target/behavior_packs/dragons.v14"
+cp -r ./addons/dragons.v14/resource "$target/resource_packs/dragons.v14"
+
+cd ~
+rm -rf my-minecraft
