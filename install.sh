f46f76d52e7b9e7e2b64161f1ab48c3ec30e559b
David Townshend <aquavitae69@gmail.com>
2022-08-21 13:08:48 +0200

    fixups

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 set -e -o pipefail
 
 rm -rf ~/my-minecraft
-git clone https://github.com/aquavitae/minecraft.git -C ~/my-minecraft
-cd ~/my-minecraft
+git clone https://github.com/aquavitae/minecraft.git
+cd ~/minecraft
 
 target="~/minecraftbe/mycraft"
 
@@ -16,6 +16,6 @@ cp -r ./addons/dragons.v14/behavior "$target/behavior_packs/dragons.v14"
 cp -r ./addons/dragons.v14/resource "$target/resource_packs/dragons.v14"
 
 cd ~
-rm -rf my-minecraft
+rm -rf minecraft
 
 $target/restart.sh
