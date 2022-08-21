44d277340874bb4fd8dcce96698c7f0e35a60512
David Townshend <aquavitae69@gmail.com>
2022-08-21 13:13:04 +0200

    fixes

--- install.sh
+++ install.sh
@@ -2,14 +2,15 @@
 
 set -e -o pipefail
 
-rm -rf ~/my-minecraft
+rm -rf ~/minecraft
 git clone https://github.com/aquavitae/minecraft.git
 cd ~/minecraft
 
 target="~/minecraftbe/mycraft"
 
-rm -rf "$target/behavior_packs/*"
-rm -rf "$target/resource_packs/*"
+rm -rf "$target/behavior_packs/Pets"
+rm -rf "$target/behavior_packs/dragons.v14"
+rm -rf "$target/resource_packs/dragons.v14"
 
 cp -r ./behavior "$target/behavior_packs/Pets"
 cp -r ./addons/dragons.v14/behavior "$target/behavior_packs/dragons.v14"
