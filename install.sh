ac0c41f623cd85f127dfa11ba6e739fdacba5874
David Townshend <aquavitae69@gmail.com>
2022-08-26 19:00:09 +0200

    update script

--- install.sh
+++ install.sh
@@ -7,12 +7,13 @@ curl https://rclone.org/install.sh | sudo bash
 
 export RCLONE_FTP_PASS=$(rclone obscure "$PLAIN_PASSWORD")
 
+rclone purge :ftp:behavior_packs
 rclone mkdir :ftp:behavior_packs/Pets
-rclone sync ./behavior :ftp:behavior_packs/Pets
+rclone copy behavior :ftp:behavior_packs/Pets
 
 for f in addons/*; do
-    rclone sync ./addons/$f/behavior :ftp:behavior_packs/$f
-    rclone sync ./addons/$f/resource :ftp:resource_packs/$f
+    rclone copy ./addons/$f/behavior :ftp:behavior_packs/$f
+    rclone copy ./addons/$f/resource :ftp:resource_packs/$f
 done
 
 rclone copy world :ftp:worlds/Legoland
