f9862ca51c415709ae43688933a50f785d335b40
David Townshend <aquavitae69@gmail.com>
2022-08-26 19:44:16 +0200

    nasty hack because rclone is dumb

--- install.sh
+++ install.sh
@@ -7,18 +7,18 @@ curl https://rclone.org/install.sh | sudo bash
 
 export RCLONE_FTP_PASS=$(rclone obscure "$PLAIN_PASSWORD")
 
-rclone purge :ftp:development_behavior_packs || 0
-rclone purge :ftp:development_resource_packs || 0
-rclone mkdir :ftp:development_behavior_packs || 0
-rclone mkdir :ftp:development_resource_packs || 0
+rclone purge :ftp:development_behavior_packs || echo "ok"
+rclone purge :ftp:development_resource_packs || echo "ok"
+rclone mkdir :ftp:development_behavior_packs || echo "ok"
+rclone mkdir :ftp:development_resource_packs || echo "ok"
 
 rclone mkdir :ftp:development_behavior_packs/Pets
-rclone copy behavior :ftp:development_behavior_packs/Pets || 0
+rclone copy behavior :ftp:development_behavior_packs/Pets || echo "ok"
 
 for f in addons/*; do
-    rclone copy ./addons/$f/behavior :ftp:development_behavior_packs/$f || 0
-    rclone copy ./addons/$f/resource :ftp:development_resource_packs/$f || 0
+    rclone copy ./addons/$f/behavior :ftp:development_behavior_packs/$f || echo "ok"
+    rclone copy ./addons/$f/resource :ftp:development_resource_packs/$f || echo "ok"
 done
 
-rclone copy world :ftp:worlds/Legoland || 0
-rclone copy settings :ftp: || 0
+rclone copy world :ftp:worlds/Legoland || echo "ok"
+rclone copy settings :ftp: || echo "ok"
