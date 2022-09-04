d11538bfa3e4ed17a4322f21f792c2d2ffbfa62d
David Townshend <aquavitae69@gmail.com>
2022-09-04 16:25:30 +0200

    retries 1

--- install.sh
+++ install.sh
@@ -15,10 +15,10 @@ rclone mkdir :ftp:development_resource_packs || echo "ok"
 for path in addons/*; do
   f="$(basename $path)"
   rclone mkdir :ftp:development_behavior_packs/$f
-  rclone copy --retries=0 ./addons/$f/behavior :ftp:development_behavior_packs/$f || echo "ok"
+  rclone copy --retries=1 ./addons/$f/behavior :ftp:development_behavior_packs/$f || echo "ok"
   rclone mkdir :ftp:development_resource_packs/$f
-  rclone copy --retries=0 ./addons/$f/resource :ftp:development_resource_packs/$f || echo "ok"
+  rclone copy --retries=1 ./addons/$f/resource :ftp:development_resource_packs/$f || echo "ok"
 done
 
-rclone copy --retries=0 world :ftp:worlds/Legoland || echo "ok"
-rclone copy --retries=0 settings :ftp: || echo "ok"
+rclone copy --retries=1 world :ftp:worlds/Legoland || echo "ok"
+rclone copy --retries=1 settings :ftp: || echo "ok"
