cd06eff1ced16152125d3d46b151ee5e1eb1baa9
David Townshend <aquavitae69@gmail.com>
2022-09-04 19:22:37 +0200

    reduce ftp noise, I hope

--- install.sh
+++ install.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-set -e -o pipefail
-set -v
+set -eo pipefail
 
 curl https://rclone.org/install.sh | sudo bash
 
 export RCLONE_FTP_PASS=$(rclone obscure "$PLAIN_PASSWORD")
 
-rclone purge :ftp:development_behavior_packs || echo "ok"
-rclone purge :ftp:development_resource_packs || echo "ok"
-rclone mkdir :ftp:development_behavior_packs || echo "ok"
-rclone mkdir :ftp:development_resource_packs || echo "ok"
+function rc() {
+  set +eo pipefail
+
+  echo ">>> rclone $@"
+  output=$(rclone $@ | grep -P "^<3>ERROR : " | grep -v 'SetModTime: 550 Not enough privileges')
+  if [ output != "" ]; then
+    echo output
+    return 1
+  fi
+}
+
+rc purge :ftp:development_behavior_packs
+rc purge :ftp:development_resource_packs
+rc mkdir :ftp:development_behavior_packs
+rc mkdir :ftp:development_resource_packs
 
 for path in addons/*; do
   f="$(basename $path)"
-  rclone mkdir :ftp:development_behavior_packs/$f
-  rclone copy --retries=1 ./addons/$f/behavior :ftp:development_behavior_packs/$f || echo "ok"
-  rclone mkdir :ftp:development_resource_packs/$f
-  rclone copy --retries=1 ./addons/$f/resource :ftp:development_resource_packs/$f || echo "ok"
+  rc mkdir :ftp:development_behavior_packs/$f
+  rc copy --retries=1 ./addons/$f/behavior :ftp:development_behavior_packs/$f
+  rc mkdir :ftp:development_resource_packs/$f
+  rc copy --retries=1 ./addons/$f/resource :ftp:development_resource_packs/$f
 done
 
-rclone copy --retries=1 world :ftp:worlds/Legoland || echo "ok"
-rclone copy --retries=1 settings :ftp: || echo "ok"
+rc copy --retries=1 world :ftp:worlds/Legoland
+rc copy --retries=1 settings :ftp:
